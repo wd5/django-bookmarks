bf11f72119611ecff73ecaf627b38cf2ea69b90c
Dan <dan.tyan@gmail.com>
2012-12-12 00:38:09 +0600

    added required lib

--- models.py
+++ models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.conf import settings
 from django.utils.html import strip_tags
+from django.utils.translation import ugettext_lazy as _
 
 
 from mptt.models import MPTTModel, TreeForeignKey
@@ -16,8 +17,12 @@ class BookmarksPost( CommonPost ):
     category = models.ManyToManyField( 
         BookmarksCategory,
         related_name = "%(app_label)s_%(class)s_related",
+        verbose_name = _( 'category' )
     )
-    site = models.URLField( null = True )
+    site = models.URLField( 
+        null = True,
+        verbose_name = _( 'site' )
+    )
 
     def hostname( self ):
         o = urlparse( self.site )
